691650e1a7f45bd6a054ba3ce348692b0644669e
marszhanglin <780965203@qq.com>
2015-06-02 08:25:10 +0800

    登陆

--- androidEcssp/src/net/evecom/androidecssp/activity/WelcomeActivity.java
+++ androidEcssp/src/net/evecom/androidecssp/activity/WelcomeActivity.java
@@ -1,9 +1,11 @@
 package net.evecom.androidecssp.activity;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import net.evecom.androidecssp.R;
 import net.evecom.androidecssp.base.BaseActivity;
+import net.evecom.androidecssp.base.ICallback;
 import net.evecom.androidecssp.util.HttpUtil;
 import net.evecom.androidecssp.util.PhoneUtil;
 import net.evecom.androidecssp.util.ShareUtil;
@@ -16,6 +18,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -62,9 +66,7 @@ public class WelcomeActivity extends BaseActivity {
 	 */
 	private void initView(){
 		findbyId();
-		iflogin();
-		
-		Log.v("mars", "自动登入");
+		iflogin(); 
 	}  
 	/**
 	 * 获取所有有用控件byid
@@ -131,6 +133,7 @@ public class WelcomeActivity extends BaseActivity {
 		}
 		//记住密码且自动登陆时提及登陆请求
 		if(autologin.equals("1")&&rembernp.equals("1")){
+			Log.v("mars", "自动登入");
 			loginsubmit(username,password);
 		} 
 	}
@@ -142,30 +145,37 @@ public class WelcomeActivity extends BaseActivity {
 		//打开进度条
 		loginProgressDialog = ProgressDialog.show(this, "提示", "正在登入，请稍等...");
 		loginProgressDialog.setCancelable(true);
-		Log.v("mars", EncryptUtil.getInstance().AESencode(password.trim()));
+//		Log.v("mars", EncryptUtil.getInstance().AESencode(password.trim()));//加密
+		
+		
+		
+		
 		//打开线程
-		 new Thread(new Runnable() { 
+		 new Thread(new Runnable() {
 			@Override
 			public void run() {
 				Log.v("mars", EncryptUtil.getInstance().AESencode(password.trim()));
 				try {
+					//登陆线程通信实体  在线程中创建
+					Message loginMessage=new Message();
+					
 					loginResult = connServerForResultPost(HttpUtil.getPCURL(getApplicationContext())
 							+"jfs/mobile/androidIndex/login",
 							"username="+username
-							+"&password="+EncryptUtil.getInstance().AESencode(password.trim())
+							+"&password="+password.trim()
 							+"&imei="+PhoneUtil.getInstance().getImei(getApplicationContext()));
 					//如果接受到返回数据 否则消息提示
 					if(loginResult.length()>5){
 						//如果登陆成功跳转  否则消息提示
 						if(loginResult.substring(0, 5).equals("true@")){
-							
+							loginMessage.what=MESSAGETYPE_01;//成功登陆
 						}else{
-							
+							loginMessage.what=MESSAGETYPE_02;//登陆失败 有接收到数据
 						}
 					}else{
-						
+						loginMessage.what=MESSAGETYPE_03;//登陆失败  没有接收到数据
 					}
-					
+					loginRequestHandler.sendMessage(loginMessage);
 					
 					Log.v("mars", "WelcomeActivity@loginResult:"+loginResult+PhoneUtil.getInstance().getImei(getApplicationContext()));
 				} catch (ClientProtocolException e) {
@@ -178,17 +188,87 @@ public class WelcomeActivity extends BaseActivity {
 	}
 
 	/**
+	 * 异步登陆请求GUI更新
+	 */
+	private  Handler loginRequestHandler=new Handler(){
+		//重写handlerMessage  
+		@Override
+		public void handleMessage(Message msg) {
+			Editor editor= passnameSp.edit();
+			switch (msg.what) {
+			case MESSAGETYPE_01:
+				//关闭登录进度条
+				if (null != loginProgressDialog) {
+					loginProgressDialog.dismiss();
+                }
+				//存储数据--用户名，密码
+				String username=userNmaeEditText.getText().toString();
+				String password=passwordEditText.getText().toString();
+				editor.putString("username", username);
+				editor.putString("password", password);
+				
+				//分割存储--user org
+				String[] loginResults = Pattern.compile(HttpUtil.DELIMITER).split(loginResult);
+				editor.putString("userid", loginResults[1]); 
+				editor.putString("usernameCN", loginResults[2]); 
+				editor.putString("sex", loginResults[3]); 
+				editor.putString("mobile_In_clound", loginResults[4]); 
+				editor.putString("orgid", loginResults[5]); 
+				editor.putString("orgname", loginResults[6]);  
+				
+				
+				Intent intent = new Intent();
+			    intent.setClass(WelcomeActivity.this, MainMenuActivity.class);
+			    startActivity(intent);
+			    WelcomeActivity.this.finish();
+				editor.commit(); 
+				Log.v("mars", "成功登陆");
+				break;
+			case MESSAGETYPE_02:
+				//关闭登录进度条
+				if (null != loginProgressDialog) {
+					loginProgressDialog.dismiss();
+                }
+				DialogToast(loginResult, new ICallback() { 
+					@Override
+					public Object execute() { 
+						loginResult="";
+						toast("ok", 1);
+						return null;
+					}
+				});
+				Log.v("mars", "登陆失败 有接收到数据");
+				break;
+			case MESSAGETYPE_03:
+				//关闭登录进度条
+				if (null != loginProgressDialog) {
+					loginProgressDialog.dismiss();
+                }
+				DialogToast(loginResult, new ICallback() { 
+					@Override
+					public Object execute() { 
+						loginResult="";
+						toast("ok", 1);
+						return null;
+					}
+				});
+				Log.v("mars", "登陆失败  没有接收到数据");
+				break;
+			default:
+				break;
+			}
+		}
+		
+	} ;
+	
+	/**
 	 * 登陆点击
 	 * @param view
 	 */
-	public void welcomelogin(View view) { 
+	public void welcomelogin(View view) {
 		String username=userNmaeEditText.getText().toString();
 		String password=passwordEditText.getText().toString();
 		loginsubmit(username,password);
-		Intent intent = new Intent();
-	    intent.setClass(this, MainMenuActivity.class);
-	    startActivity(intent);
-	    this.finish();
 	}
 	
 
